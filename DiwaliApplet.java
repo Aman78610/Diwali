a967508740e0553f9ba559c6b8bfbc6abdb7310e
Aman78610 <44833040+Aman78610@users.noreply.github.com>
2018-11-07 14:41:23 +0530

    Create DiwaliApplet.java

---
+++ DiwaliApplet.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.applet.*;
+
+import javax.imageio.ImageIO;
+
+/*
+<applet code="Diwali" height="300" width="500">
+		
+	</applet>
+*/
+
+public class Diwali extends Applet{
+	
+	
+	Image pic1,pic2;  
+	  
+	  public void init() {  
+		  pic1 = getImage(getDocumentBase(), "images.png");
+		  pic2 = getImage(getDocumentBase(), "d.jpg");
+         
+	  }
+	
+	
+	public void paint(Graphics g)
+	{	
+		setBackground(Color.red);
+		
+		 g.drawImage(pic1, 30,30, this);
+		 g.drawImage(pic2, 30,200, this);
+		
+		
+	}
+}
